b7f918c827a2f35db76858f4da680980a6333dc9
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-05-29 20:43:02 +0530

    Practice que

---
+++ Javascript/Day 1/prac3.js
@@ -0,0 +1,15 @@
+let str="apple";
+if(str[0]==='a' && str.length>3 ){
+    console.log("good String")
+}
+else{
+    console.log("Not a good string")
+}
+
+let num=12;
+if((num%3===0)&&((num+1==15)||(num-1==11))) {
+    console.log("Safe")
+}
+else{
+    console.log("unsafe")
+}
