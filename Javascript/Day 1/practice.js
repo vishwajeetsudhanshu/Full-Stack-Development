b40bc51c9bf2195eee5fe845af34921c2350f752
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-05-29 18:15:08 +0530

    Practice

---
+++ Javascript/Day 1/practice.js
@@ -0,0 +1,10 @@
+let color="red";
+if(color==="red"){
+    console.log("Stop ! light color is red")
+}
+if(color==="Yellow"){
+    console.log("Be Ready !")
+}
+if(color==="Green"){
+    console.log("GO !")
+}
