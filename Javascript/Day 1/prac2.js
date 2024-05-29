a4ab2d7d906ac719aa63a8d9c9f2a6fb32e606fe
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-05-29 18:36:53 +0530

    practice ques

---
+++ Javascript/Day 1/prac2.js
@@ -0,0 +1,16 @@
+let size='o';
+if(size==='XL'){
+    console.log(`the price is 250 rupees`)
+}
+else if(size==='L'){
+    console.log(`the price is 200 rupees`);
+}
+else if(size==='M'){
+    console.log(`the price is 100 rupees`);
+}
+else if(size==='S'){
+    console.log(`the price is 50 rupees`);
+}
+else{
+    console.log(`Ask shopkeeper`);
+}
