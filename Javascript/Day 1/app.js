0e11761d53ef923b6db4bbf4341dcfa3955d7d1a
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-05-29 17:42:11 +0530

    Operators

--- Javascript/Day 1/app.js
+++ Javascript/Day 1/app.js
@@ -2,5 +2,28 @@
 //console.log("Sudhansoo")
 let a=10;
 let b=5;
-console.log("Sum is:",a+b)
+console.log(" Sum is: ", a+b );
 //this is comment
+let pencilPrice=10;
+let erasorPrice=5;
+//let output("the total price is :"+(pencilPrice+erasorPrice)+"Rupees. ");
+let output=`the total price is: ${pencilPrice+erasorPrice} Rupees.`;
+console.log(output); 
+//Arithmetic Operators
+let p=15;
+let q=5;
+console.log(`sum is ${p+q}`);
+console.log(p - q);
+console.log(p * q);
+console.log(p / q);
+console.log(p % q);
+console.log(p ** q);
+console.log(p++);//15
+console.log(++p);//17
+
+//comparison operator
+let age=18;
+
+
+
+
