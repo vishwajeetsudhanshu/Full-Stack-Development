381a4778b304384c98b437cbb8da38a756d97d6f
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-05-29 20:56:02 +0530

    switch statement

---
+++ Javascript/Day 1/switch.js
@@ -0,0 +1,15 @@
+let color="redish";
+switch(color){
+    case "red":
+        console.log("stop");
+        break;
+    case "yellow":
+        console.log("be ready");
+        break;
+    case "green":
+        console.log("go");
+        break;
+    default:
+        console.log("broken");
+        break;
+}
