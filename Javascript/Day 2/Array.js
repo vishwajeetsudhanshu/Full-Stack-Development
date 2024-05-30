406a303bae80ddb6ff4e3394fe9407867d64fd7d
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-05-30 21:57:25 +0530

    Array & it''s methods

---
+++ Javascript/Day 2/Array.js
@@ -0,0 +1,20 @@
+let std=["Aman","Ananad","rajat"];
+console.log("Array student",std);
+let nums=[2,4,6,9];
+console.log("At zero index nums[0]",nums[0]);
+console.log("length of num nums.length",nums.length);
+//mixed array
+let info=["shardha",65,45,2.5];
+console.log(info)
+let emparr=[];
+console.log(emparr);
+//to acces character
+console.log(info[0][0]);
+console.log("length of first array",info[0].length);
+//Array are mutable
+let fruits=["mango","apple","litchi"];
+fruits[0]="Banana";
+console.log(fruits);
+fruits[15]="papaya";
+console.log(fruits);
+//Array Methods
