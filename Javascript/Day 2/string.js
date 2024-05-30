d395ec5a19b52580f9c43dbd6d900de3a62dc461
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-05-30 20:48:31 +0530

    String & it's methods

---
+++ Javascript/Day 2/string.js
@@ -0,0 +1,25 @@
+let str="    apna   ";
+str.trim();
+//let msg=prompt("ENter your password");
+//let pass=msg.trim();
+//console.log(pass);
+//console.log(msg);
+let name="Apna college";
+console.log(name.toUpperCase() );
+let a="I Love Coding"
+console.log(a.indexOf("Love"));
+console.log(a.indexOf("o"));
+console.log(a.indexOf("y"));
+console.log(a.indexOf("ove"));
+let msg="   Hello   ";
+let newMsg=msg.trim();
+console.log("after trim: ",newMsg);
+newMsg=newMsg.toUpperCase();
+console.log("agter upper",newMsg);
+console.log(msg.trim().toUpperCase());
+let string="ILoveCoding";
+console.log(string.slice(2,4));
+console.log(string.slice(4));
+console.log(string.slice(-2));//11-2=9
+//Replace Methods
+
