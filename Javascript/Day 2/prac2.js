26ea4bad0e580ecc209596df4bb5b3df7ff1a02e
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-05-31 22:36:19 +0530

    practice ques

---
+++ Javascript/Day 2/prac2.js
@@ -0,0 +1,8 @@
+let start=['january','july','march','august'];
+console.log("After splice");
+console.log(start.slice(0,2,"july","june"));
+
+//que2
+lang=['c','c++','html','javascript','python','java','c#','sql'];
+console.log(lang);
+console.log("After reverse index of javascript",lang.reverse().indexOf("javascript"));
