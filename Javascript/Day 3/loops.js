3ebb50cc649b09e9d80c999fcb2ebf451882fe30
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-03 19:23:38 +0530

    Loops

--- Javascript/Day 3/loops.js
+++ Javascript/Day 3/loops.js
@@ -1,4 +1,8 @@
 let i=1;
 for(i;i<=5;i++){
     console.log(i)
+};
+let j=10;
+for(j;j>=0;j=j-2){
+    console.log("Backward Loops",j)
 }
---
+++ Javascript/odd.js
@@ -0,0 +1,4 @@
+let i=1;
+for(i;i<=15;i=i+2){
+    console.log(i)
+}
