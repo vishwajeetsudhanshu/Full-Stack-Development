523ad834f133883863e671a5656ac15462aa99f6
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-03 18:31:30 +0530

    Practice Ques

--- Javascript/Day 2/prac3.js
+++ Javascript/Day 2/prac3.js
@@ -1,1 +1,6 @@
-let game=[['X',null,'0']]
+let game=[['X',null,'0'],[null,'X','0'],['0',null,'X']]
+console.log(game);
+console.log(game[0][1]);
+console.log(game[0]);
+console.log(game[0][1]='0');
+console.log(game);
