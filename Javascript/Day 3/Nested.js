8576f51dd811850b1652f9a4420c460d55105cea
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-04 12:21:26 +0530

    Loops

---
+++ Javascript/Day 3/Nested.js
@@ -0,0 +1,6 @@
+let i=1;
+for(i;i<=5;i++){
+    console.log(`outer loop ${i}`);
+    for(let j=1;j<=5;j++){
+    console.log(j)}
+};
---
+++ Javascript/Day 3/while.js
@@ -0,0 +1,5 @@
+let i=100;
+while(i <=500){
+    console.log(i);
+    i=i+50;
+}
