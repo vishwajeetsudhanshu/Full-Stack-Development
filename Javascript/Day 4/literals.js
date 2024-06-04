c6a60b4b6d5c6b292b19d08543b9f40798db7cb4
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-04 17:27:11 +0530

    Object Literals

---
+++ Javascript/Day 4/literals.js
@@ -0,0 +1,11 @@
+const student={name:"shradha",age:23,marks:94};
+console.log(student);
+console.log("Post");
+const post={
+    username:"@sudhansoo",
+    content:"This is my first contibution",
+    likes:150,
+    reposts:50,
+    tags:['@sanny','@sudhansoo']
+}
+console.log(post)
