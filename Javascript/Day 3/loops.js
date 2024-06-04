93ecd80c3e4af7b01415b016bf07fa7cbe15a970
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-04 13:16:06 +0530

    Loops

--- Javascript/Day 3/loops.js
+++ Javascript/Day 3/loops.js
@@ -6,3 +6,19 @@ let j=10;
 for(j;j>=0;j=j-2){
     console.log("Backward Loops",j)
 }
+
+
+const fav_movie="RRR";
+let guess=prompt("Guess My fav movie");
+while(guess!=fav_movie ){
+    if(guess=="quit") {
+    console.log(" U Quit! Better Luck Next time!");
+    break;}
+    guess=prompt("Wrong guess Try Again!");
+    console.log(guess)
+
+
+}
+if(guess==fav_movie){
+    console.log("Congratulation");
+}
