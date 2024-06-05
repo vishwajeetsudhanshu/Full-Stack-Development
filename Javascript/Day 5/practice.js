d329f474a86b0d7ffaa57ef836e12f450f94b744
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-05 19:39:47 +0530

    Practice Ques

---
+++ Javascript/Day 5/practice.js
@@ -0,0 +1,18 @@
+function priPoem(){
+    console.log("Jhonny Jhonny yes Papa!")
+    console.log("Eating sugar No Papa !");
+    console.log("Telling a Lie No Papa!");
+    console.log("Open your mouth Ha HA Ha!");
+}
+priPoem();
+
+function rollDice(){
+    let rad=Math.floor(Math.random()*6)+1;
+    console.log(rad);
+}
+rollDice();
+rollDice();
+rollDice();
+rollDice();
+rollDice();
+rollDice();
