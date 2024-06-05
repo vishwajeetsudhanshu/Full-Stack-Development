f146b98a366a32ca5287010f67a15ddcf2f419d1
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-05 18:10:22 +0530

    Objects Literals(Add/update/Get values/object of objects)

--- Javascript/Day 4/literals.js
+++ Javascript/Day 4/literals.js
@@ -1,5 +1,10 @@
-const student={name:"shradha",age:23,marks:94};
+const student={name:"shradha",age:23,marks:94,city:"Delhi"};
 console.log(student);
+student.city="Mumbai"
+student.gender="Female";
+student.marks="A";
+delete student.marks
+console.log(student)
 console.log("Post");
 const post={
     username:"@sudhansoo",
---
+++ Javascript/Day 4/object.js
@@ -0,0 +1,24 @@
+const classInfo={
+    aman:{
+        Full_Name:"Amn khana",
+        Gender:"Male",
+        Marks:"A+",
+        City:"Mumbai",
+        CGPA:9
+    },
+    anu:{
+        Full_Name:"Anu khana",
+        Gender:"Female",
+        Marks:"A+",
+        City:"Mumbai",
+        CGPA:9
+    },
+    anku:{
+        Full_Name:"Anu khana",
+        Gender:"Female",
+        Marks:"A+",
+        City:"Mumbai",
+        CGPA:9
+    }
+}
+console.log(classInfo)
