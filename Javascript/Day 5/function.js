73723d07afb6769c3715d820606a39dffa42c4ce
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-05 19:30:52 +0530

    Function

---
+++ Javascript/Day 5/function.js
@@ -0,0 +1,25 @@
+function hello(){
+    console.log("Hello !");
+}
+hello();
+
+function priName(){
+    console.log("Sudhansoo")
+}
+priName();
+function pri1to5(){
+    for(let i=0;i<=5;i++){
+        console.log(i);
+    }
+}
+pri1to5();
+function isAdult(){
+    let age=15;
+    if(age>=18){
+        console.log("Adult")
+    }
+    else{
+        console.log("Not Eligible")
+    }
+}
+isAdult();
