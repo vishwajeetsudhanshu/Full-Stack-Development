ae5faa889894830b284596ad418cb1acb9d54ecf
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-05 18:16:07 +0530

    Array of objects

---
+++ Javascript/Day 4/array.js
@@ -0,0 +1,24 @@
+const classInfo=[
+    {
+        Full_Name:"Amn khana",
+        Gender:"Male",
+        Marks:"A+",
+        City:"Mumbai",
+        CGPA:9
+    },
+    {
+        Full_Name:"Anu khana",
+        Gender:"Female",
+        Marks:"A+",
+        City:"Mumbai",
+        CGPA:9
+    },
+    {
+        Full_Name:"Anu khana",
+        Gender:"Female",
+        Marks:"A+",
+        City:"Mumbai",
+        CGPA:9
+    }
+]
+console.log(classInfo);
