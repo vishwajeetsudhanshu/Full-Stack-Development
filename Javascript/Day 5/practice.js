539a08ae461c924050a692caea9370362ac787a8
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-06 21:54:32 +0530

    Practice Ques

--- Javascript/Day 5/practice.js
+++ Javascript/Day 5/practice.js
@@ -16,3 +16,11 @@ rollDice();
 rollDice();
 rollDice();
 rollDice();
+
+
+function avg(num1,num2,num3){
+    
+    console.log((num1+num2+num3)/3);
+}
+console.log("Avg of 3 num 4,6,8 is");
+avg(4,6,8)
