535bd4f99c0087e7db5f199ad2f21da079a59463
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-13 19:13:41 +0530

    Return Keyword

--- Javascript/Day 5/return.js
+++ Javascript/Day 5/return.js
@@ -3,3 +3,21 @@ function avg(num1,num2,num3){
     return((num1+num2+num3)/3);
 }
 console.log("Avg of 3 num 4,6,8 is",avg(4,6,8));
+
+function sum(a,b){
+    return a+b;
+    console.log(hello);
+}
+console.log(sum(sum(3,4),9));
+
+function isAdult(age){
+    if(age>=18){
+        return"Adult";
+    }
+    else{
+        return"Not Adult";
+        
+    }
+    console.log(hello);
+}
+console.log(isAdult(35));
