321f0c36d5cdb2cf1e4e96e6f548c0e509727782
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-14 16:52:40 +0530

    Methods

---
+++ Javascript/Day 5/method.js
@@ -0,0 +1,18 @@
+const cal={
+    num:55,
+    add:function(a,b){
+        return a+b;
+    },
+    sub:function(a,b){
+        return a-b;
+    },
+
+    mul:function(a,b){
+         return a*b;
+    },
+
+}
+console.log(cal);
+console.log(cal.add(4,5));
+console.log(cal.sub(9,5));
+console.log(cal.mul(8,8));
