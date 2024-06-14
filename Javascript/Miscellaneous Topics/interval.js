9c378d59d6069319ba5e99aeb2c422516b91b8aa
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-14 19:17:43 +0530

    setInterval

---
+++ Javascript/Miscellaneous Topics/interval.js
@@ -0,0 +1,11 @@
+console.log("Hi there !");
+
+let id=setInterval(() => {console.log("Sudhansoo");
+},1000);
+console.log("Welcome ")
+console.log(id)
+let id2=setInterval(() => {console.log("Sanny Sudhansoo");
+},5000);
+console.log("Welcome ")
+console.log(id2)
+clearInterval(id);
