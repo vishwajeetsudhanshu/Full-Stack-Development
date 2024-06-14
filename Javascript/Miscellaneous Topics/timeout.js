f4eba6a03cf9b8094754820c5a4bde1cb997e154
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-14 19:09:33 +0530

    setTimeout

---
+++ Javascript/Miscellaneous Topics/timeout.js
@@ -0,0 +1,12 @@
+console.log("Hi there !");
+
+setTimeout(()=>{
+    console.log("Sudhansoo");
+},4000);
+console.log("Welcome ")
+console.log("Welcome ")
+console.log("Welcome ")
+console.log("Welcome ")
+console.log("Welcome ")
+console.log("Welcome ")
+console.log("Welcome ")
