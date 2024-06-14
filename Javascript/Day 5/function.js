0e7783e34e35fe033fba1120f6454aa9d68e0139
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-14 16:42:55 +0530

    Functions

--- Javascript/Day 5/function.js
+++ Javascript/Day 5/function.js
@@ -29,3 +29,39 @@ const sum=function(a,b){
     return a+b;
 }
 console.log(sum(4,5));
+
+
+console.log("High Order Functions");
+function multiplegreet(func,count){
+    for(let i=1;i<=count;i++){
+        func();
+    }
+}
+let greet=function(){
+    console.log("hello");
+}
+multiplegreet(greet,10);
+
+console.log("High Order Functions-Reurns a functions");
+function oddeventest(request){
+    if(request=="odd"){
+        let odd=function(n){
+            console.log(!(n%2==0));
+        }
+        return odd;
+    }
+    else if(request=="even"){
+       let even= function(n){
+            console.log(n%2==0);
+        }
+        return even;
+    }
+    else{
+        console.log("Error");
+    }
+     
+}
+let testOdd = oddeventest("odd");
+let testEven = oddeventest("even");
+testOdd(3);
+testEven(9);
