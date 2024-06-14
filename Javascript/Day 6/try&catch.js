f9c11c94ae8796fc084a1a5a1d49dbd5cf61f94e
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-14 18:41:52 +0530

    try&catch

---
+++ Javascript/Day 6/try&catch.js
@@ -0,0 +1,12 @@
+console.log("Hello");
+console.log("Hello2");
+
+try{
+    console.log(a);
+}
+catch(err){
+    console.log("Caught an error   a is not defined");
+    console.log(err);
+}
+
+console.log("Hello3");
