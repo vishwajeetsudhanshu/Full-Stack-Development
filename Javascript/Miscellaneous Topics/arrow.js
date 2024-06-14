527654649cb02242404907f71404f7e33180b725
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-14 18:55:35 +0530

    Arrow Functions

---
+++ Javascript/Miscellaneous Topics/arrow.js
@@ -0,0 +1,18 @@
+const sum=(a,b)=>{
+    console.log(a+b);
+}
+sum(2,3);
+const cube=n=>{
+    return n*n*n;
+}
+console.log(`The cube  is `,cube(2));
+const pow=(x,y)=>{
+    return x**y;
+
+}
+console.log(pow(2,3));
+
+const hello=()=>{
+    console.log("Hello");
+}
+hello();
