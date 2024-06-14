8285877b436b7306fab182953c0ccd76c544fee9
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-14 16:56:07 +0530

    Methods

--- Javascript/Day 5/method.js
+++ Javascript/Day 5/method.js
@@ -16,3 +16,10 @@ console.log(cal);
 console.log(cal.add(4,5));
 console.log(cal.sub(9,5));
 console.log(cal.mul(8,8));
+//2nd method
+const calc={
+    aad(x,y){
+        return x+y;
+    }
+}
+console.log(calc.aad(8,9));
