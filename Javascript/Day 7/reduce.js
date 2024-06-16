34ca2c593eb306b5919205d9404fcd3aa615c66f
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-16 09:59:08 +0530

    Reduce Function

--- Javascript/Day 7/reduce.js
+++ Javascript/Day 7/reduce.js
@@ -1,6 +1,7 @@
 console.log("Reduce Function");
 let arrr=[2,4,9,8];
-let reduce=arrr.reduce((red,el)=>
-     red+el
-);
+let reduce=arrr.reduce((red,el)=>{
+     console.log(red);
+     return red+el
+});
 console.log(reduce);
