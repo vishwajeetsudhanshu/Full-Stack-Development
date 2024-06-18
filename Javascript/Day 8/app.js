a7719196db2102a82f14364f79d96b8a1da4c783
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-18 15:51:55 +0530

    Selecting Elements

---
+++ Javascript/Day 8/app.js
@@ -0,0 +1,9 @@
+let smallimg=document.getElementsByClassName("oldImg");
+for(let i=0;i<smallimg.length;i++){
+    console.dir(smallimg[i]);
+    console.dir(smallimg[i].src);
+    smallimg[i].src="img/Spider.jpg";
+    console.log(smallimg[i]);
+    console.log(`value of image no. ${[i]} is changed`)
+
+}                                                        
