293c7c54f179f0d987f5b39d5f7767e6582bbb88
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-18 18:58:25 +0530

    updated

--- Javascript/Day 8/app.js
+++ Javascript/Day 8/app.js
@@ -16,3 +16,27 @@ console.dir(firp);
 console.dir(document.querySelector(".oldImg"));
 console.dir(document.querySelectorAll(".oldImg"));
 console.dir(document.querySelectorAll("div a"));
+console.log("Manipulating the object");
+let para=document.querySelector('p');
+console.log(para);
+console.dir(para);
+para.innerText="Hi I'm Peter";
+para.innerHTML="Hi I'm <b>Peter Parkar</b>";
+let  head=document.querySelector('h1');
+//head.innerHTML="<u>Spider-Man</u>";
+head.innerHTML=`<u>${head.innerText}</u>`;
+let img=document.querySelector('img');
+console.log(img);
+console.dir(img);
+img.getAttribute('id');
+img.setAttribute('id','Spider-Man');
+img.setAttribute('src','img/1.jpg');
+console.log("Manipulating Styles");
+console.log(img.style);
+console.log(head.style);
+console.log(head.style.color='green');
+console.log(head.style.backgroundColor='pink')
+let links=document.querySelectorAll(".box a");
+for(let i=0;i<links.length;i++){
+    console.log(links[i].style.color='purple');//inline style
+}
