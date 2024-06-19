8b43a0bdd8ac1238dcbeec30a650a0ce84dd5c3a
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-19 23:02:40 +0530

    Form Events

--- Javascript/Day 9/forms.js
+++ Javascript/Day 9/forms.js
@@ -1,5 +1,27 @@
 let form=document.querySelector('form');
 form.addEventListener("submit",function(event){
     event.preventDefault();
-    alert("Form submitted");
+//     console.dir(form);
+//     let user=this.elements[0];//form.elements[0] both are equal
+//     let pass=document.querySelector("#pass");
+//     console.log(user.value);
+//     console.log(pass.value);
+//     alert(`hi ${user.value}, your pass is set to ${pass.value}`);
+    
+
+    // let input=document.querySelector('input');
+    // console.dir(input);
+    // console.log(input.value);
+
+});
+let user=document.querySelector("#user");
+user.addEventListener("change",function(){
+    console.log("Change event");
+    console.log("final value=",user.value);
+
+});
+user.addEventListener("input",function(){
+    console.log("Input event");
+    console.log("final value=",user.value);
+
 });
