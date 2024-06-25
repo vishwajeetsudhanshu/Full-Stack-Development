2011e4427970c5bba19725f58c6c39332569d4c5
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-25 22:26:30 +0530

    Js Call Stack

--- Javascript/Day 10/app.js
+++ Javascript/Day 10/app.js
@@ -2,3 +2,22 @@ function hello(){
     console.log("Hello!");
 }
 hello();
+function functionThree() {
+    console.log("Inside functionThree");
+}
+
+function functionTwo() {
+    console.log("Inside functionTwo");
+    functionThree(); // This will push functionThree onto the call stack
+    console.log("Back to functionTwo");
+}
+
+function functionOne() {
+    console.log("Inside functionOne");
+    functionTwo(); // This will push functionTwo onto the call stack
+    console.log("Back to functionOne");
+}
+
+console.log("Starting execution");
+functionOne(); // This will push functionOne onto the call stack
+console.log("Execution finished");
