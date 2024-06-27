61749fc5a2c55105dc06e800c4202373e33ae91c
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-06-27 21:37:42 +0530

    Updated

---
+++ Node.js/Day 1/Math.js
@@ -0,0 +1,25 @@
+const sum=(a,b)=>a+b;
+const mul=(a,b)=>a*b;
+const g=9.8;
+
+// let obj={
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+// };
+
+
+// module.exports=obj;
+
+// module.exports={
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+// };
+
+
+// module.exports=obj;
+
+// module.exports sum=(a,b)=>a+b;
+// const mul=(a,b)=>a*b;
+// const g=9.8;
