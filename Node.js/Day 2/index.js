1bb086811a2a7ca32e8eeb85f11fcf6863cc9153
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-07-01 22:13:37 +0530

    Updated

--- Node.js/Day 2/index.js
+++ Node.js/Day 2/index.js
@@ -1,14 +1,34 @@
-const express=require("express");
-const app=express();
+// const express=require("express");
+// const app=express();
+
+// console.dir(app);
+
+// let port=8080;
+// app.listen(port,()=>{
+//     console.log(`app listening on port ${port}`);
+// });
+
+// app.use((req,res) => {
+//     console.log("request received");
+
+// });
+const express = require("express");
+const app = express();
 
 console.dir(app);
 
-let port=8080;
-app.listen(port,()=>{
+let port = 8080;
+app.listen(port, () => {
     console.log(`app listening on port ${port}`);
 });
 
-app.use((req,res) => {
+app.use((req, res, next) => {
     console.log("request received");
+    res.send("Request received");
+    next();
+});
 
+// Example of handling a specific route
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
