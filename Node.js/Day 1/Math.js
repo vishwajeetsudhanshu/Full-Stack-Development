4480c7aa4c8cb80de820f5c302a5afd40fca12a0
vishwajeetsudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2024-07-03 22:55:18 +0530

    Updated

--- Node.js/Day 1/Math.js
+++ Node.js/Day 1/Math.js
@@ -2,24 +2,24 @@ const sum=(a,b)=>a+b;
 const mul=(a,b)=>a*b;
 const g=9.8;
 
-// let obj={
-//     sum:sum,
-//     mul:mul,
-//     g:g,
-// };
+let obj={
+    sum:sum,
+    mul:mul,
+    g:g,
+};
 
 
-// module.exports=obj;
+module.exports=obj;
 
-// module.exports={
-//     sum:sum,
-//     mul:mul,
-//     g:g,
-// };
+module.exports={
+    sum:sum,
+    mul:mul,
+    g:g,
+};
 
 
-// module.exports=obj;
+module.exports=obj;
 
-//module.exports sum1=(a,b)=>a+b;
-// const mul2=(a,b)=>a*b;
-// const g2=9.8;
+module.exports sum1=(a,b)=>a+b;
+const mul2=(a,b)=>a*b;
+const g2=9.8;
