c70e2d91323c1c1f2833fa2d59196a1a98093e66
vishwajeetsudhanshu <vkshudhanshu4u@gmail.com>
2026-02-10 12:02:46 +0530

    COntrol Flow

---
+++ Javascript/JS/Control Flow/Basic.js
@@ -0,0 +1,29 @@
+//If Statement
+// if(condition==true){
+// //code execute
+// }
+// if(condition==false){
+// //code not execute
+// }
+//<,>,<=,>=,==,===(it check type also),!=,
+// const isUserLoggedIn=true;
+// if(isUserLoggedIn){
+
+// }
+if(2=="2"){
+    console.log("Executed")
+}
+if(2==="2"){
+    console.log("Execution")
+}
+else{
+    console.log("Not")
+}
+
+const temp=61;
+if(temp<50){
+    console.log("Temp less than 50")
+}
+else{
+    console.log("tem is greater than 50")
+}
